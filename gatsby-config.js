4fa875df61f2400221e4a995a3214624f4716e2b
melonsodastation <melonsodastation@gmail.com>
2020-03-12 20:22:11 +0900

    first commit
    - change 'gatsby-config'

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Melonsodastation`,
+    author: `KIM SANHON`,
+    description: `©melonsodastation.`,
+    siteUrl: `https://melonsodastation.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      //twitter: `kylemathews` ,
     },
   },
   plugins: [
