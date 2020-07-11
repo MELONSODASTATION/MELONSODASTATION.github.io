e935a9560a42533c712949895574ff2fbb5554d2
melonsodastation <melonsodastation@gmail.com>
2020-07-11 19:22:26 +0900

    Internationallize

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,6 +73,14 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      'gatsby-internataion'
+      resolve: 'gatsby-plugin-i18n',
+      options: {        
+        langKeyDefault: 'en',
+        useLangKeyLayout: false
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
